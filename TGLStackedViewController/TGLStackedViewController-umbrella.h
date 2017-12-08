c35d56fadaad91b4935d290359c68ae0b726f567
pakorn <pakorn@kitchenup.com>
2017-12-08 16:45:47 +0700

    Commit

---
+++ TGLStackedViewController/TGLStackedViewController-umbrella.h
@@ -0,0 +1,19 @@
+//
+//  TGLStackedViewController.h
+//  TGLStackedViewController
+//
+//  Created by Ampos on 8/12/2560 BE.
+//  Copyright © 2560 Ampos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TGLStackedViewController.
+FOUNDATION_EXPORT double TGLStackedViewControllerVersionNumber;
+
+//! Project version string for TGLStackedViewController.
+FOUNDATION_EXPORT const unsigned char TGLStackedViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGLStackedViewController/PublicHeader.h>
+
+
